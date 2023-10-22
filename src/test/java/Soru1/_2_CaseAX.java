b515cb409ce4f18d7ef5dac8d45f1f6e169e3757
AhmetCan <a.canozyurek@gmail.com>
2023-10-22 13:08:55 +0300

    AutomationExerciseTestCase2

---
+++ src/test/java/Soru1/_2_CaseAX.java
@@ -0,0 +1,44 @@
+package Soru1;
+
+import com.github.javafaker.Faker;
+import org.bouncycastle.pqc.jcajce.interfaces.FalconKey;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import utilities.TestBAse;
+
+public class _2_CaseAX extends TestBAse {
+@Test
+    public void secCase(){
+
+    //1. Launch browser
+    //2. Navigate to url 'http://automationexercise.com'
+    driver.get("http://automationexercise.com");
+    //3. Verify that home page is visible successfully
+    WebElement mainSign = driver.findElement(By.xpath("//*[@*='logo pull-left']"));
+    SoftAssert softAssert = new SoftAssert();
+    softAssert.assertTrue(mainSign.isDisplayed());
+    //4. Click on 'Signup / Login' button
+    driver.findElement(By.xpath("//i[@class='fa fa-lock']")).click();
+    //5. Verify 'Login to your account' is visible
+    WebElement loginText = driver.findElement(By.xpath("//h2[text()='Login to your account']"));
+    softAssert.assertTrue(loginText.isDisplayed());
+    //6. Enter correct email address and password
+    Faker faker = new Faker();
+    WebElement emailBox1 = driver.findElement(By.xpath("(//input[@placeholder='Email Address'])[1]"));
+    emailBox1.sendKeys("email@gmail.com");
+    WebElement passwordBox1 = driver.findElement(By.xpath("(//input[@placeholder='Password'])[1]"));
+passwordBox1.sendKeys("email.gmail");
+    //7. Click 'login' button
+driver.findElement(By.xpath("//button[@data-qa='login-button']")).click();
+    //8. Verify that 'Logged in as username' is visible
+WebElement loggedText = driver.findElement(By.xpath("//*[text()=' Logged in as ']"));
+softAssert.assertTrue(loggedText.isDisplayed());
+    //9. Click 'Delete Account' button
+    driver.findElement(By.xpath("//*[text()=' Delete Account']")).click();
+    //10. Verify that 'ACCOUNT DELETED!' is visible
+      WebElement deletedSign = driver.findElement(By.xpath("//*[text()='Account Deleted!']"));
+      softAssert.assertTrue(deletedSign.isDisplayed());
+}
+}

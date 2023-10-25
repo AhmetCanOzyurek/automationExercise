8a077f6752379da25fe6b6f0bdb26b52fe66ded2
AhmetCan <a.canozyurek@gmail.com>
2023-10-25 18:58:06 +0300

    AutomationExerciseTestCase3/IncorrectUserCredentialTest

---
+++ src/test/java/Soru1/_3_TestCase.java
@@ -0,0 +1,37 @@
+package Soru1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import utilities.TestBAse;
+
+public class _3_TestCase extends TestBAse {
+    @Test
+    public void incorrectUser(){
+        //1. Launch browser
+        //2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+        //3. Verify that home page is visible successfully
+        WebElement homePage = driver.findElement(By.xpath("//div[@class='logo pull-left']"));
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(homePage.isDisplayed());
+        //4. Click on 'Signup / Login' button
+        driver.findElement(By.xpath("//i[@class='fa fa-lock']")).click();
+        //5. Verify 'Login to your account' is visible
+        WebElement loginToYourAccountText = driver.findElement(By.xpath("//h2[text()='Login to your account']"));
+        softAssert.assertTrue(loginToYourAccountText.isDisplayed());
+        //6. Enter incorrect email address and password
+        WebElement emailBoxLogin = driver.findElement(By.xpath("(//*[@placeholder='Email Address'])[1]"));
+        emailBoxLogin.sendKeys("asdlfjslkdfjfdl@gmail.com");
+        WebElement passwordLogin = driver.findElement(By.xpath("(//*[@placeholder='Password'])[1]"));
+        passwordLogin.sendKeys("132456");
+        //7. Click 'login' button
+        driver.findElement(By.xpath("//*[@data-qa='login-button']")).click();
+        //8. Verify error 'Your email or password is incorrect!' is visible
+        WebElement errorIncorrect = driver.findElement(By.xpath("//*[text()='Your email or password is incorrect!']"));
+        softAssert.assertTrue(errorIncorrect.isDisplayed());
+    }
+}

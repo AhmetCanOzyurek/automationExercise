eb412683546e5e279faec71da416fba6c149eb9e
AhmetCan <a.canozyurek@gmail.com>
2023-11-06 22:00:20 +0300

    12.testCaseAE

---
+++ src/test/java/AE/_12_TestCase.java
@@ -0,0 +1,43 @@
+package AE;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+import java.util.List;
+
+import static utilities.Locators.*;
+
+public class _12_TestCase  extends TestBase {
+
+
+    @Test
+    public void AddProductsInCart(){
+       // 1. Launch browser
+       // 2. Navigate to url 'http://automationexercise.com'
+        navigateToSite();
+       // 3. Verify that home page is visible successfully
+        verifyMainPage(lmainPage);
+       // 4. Click 'Products' button
+        click(lProductButton);
+       // 5. Hover over first product and click 'Add to cart'
+        click(lAddToCartButton);
+       // 6. Click 'Continue Shopping' button
+       click(lContinueShopping);
+        // 7. Hover over second product and click 'Add to cart'
+        click(lAddToCartButton2);
+       // 8. Click 'View Cart' button
+        click(lWievCart);
+       // 9. Verify both products are added to Cart
+softAssert.assertTrue(driver.findElement(By.cssSelector("#product-1")).isDisplayed());
+        softAssert.assertTrue(driver.findElement(By.cssSelector("#product-2")).isDisplayed());
+       // 10. Verify their prices, quantity and total price
+        List<WebElement> cartVerifying =driver.findElements(By.cssSelector("tbody"));
+
+        for (WebElement e : cartVerifying) {
+           softAssert.assertTrue(e.isDisplayed());
+        }
+
+        softAssert.assertAll();
+    }
+}

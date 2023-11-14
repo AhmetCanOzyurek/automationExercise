3689f9cebd409c0479dd9357b38c6c4b8fe085c7
AhmetCan <a.canozyurek@gmail.com>
2023-11-14 18:01:20 +0300

    productQuantityTest

---
+++ src/test/java/AE/_13_TestCase.java
@@ -0,0 +1,34 @@
+package AE;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+import static utilities.Locators.*;
+
+public class _13_TestCase extends TestBase {
+    @Test
+    public void productQuantity(){
+       //1. Launch browser
+       //2. Navigate to url 'http://automationexercise.com'
+        navigateToSite();
+       //3. Verify that home page is visible successfully
+        verifyMainPage(lmainPage);
+        //4. Click 'View Product' for any product on home page
+        click(lviewProduct);
+       //5. Verify product detail is opened
+verifyVisibility(lviewProduct);
+       //6. Increase quantity to 4
+sendKeys(lQuantityBox, "4");
+       //7. Click 'Add to cart' button
+click(lAddtoCartButtonPrdctPage);
+       //8. Click 'View Cart' button
+click(lWievCart);
+       //9. Verify that product is displayed in cart page with exact quantity
+        String expectedQuantity = "4";
+        String actualQuantity = driver.findElement(lCartQuantityBox).getText();
+        Assert.assertEquals(expectedQuantity,actualQuantity);
+
+
+    }
+}

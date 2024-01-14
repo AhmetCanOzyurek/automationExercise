c24a2316cb6d4ac2229eeacbf66e61ae3490c72b
AhmetCan <a.canozyurek@gmail.com>
2024-01-14 17:19:25 +0300

    Revisions for my automationExercise TestNG Project. part/18

--- src/test/java/AE/TenthToTwentieth/_16_TestCase.java
+++ src/test/java/AE/TenthToTwentieth/_16_TestCase.java
@@ -1,56 +1,77 @@
 package AE.TenthToTwentieth;
 
+import Pages.CartPage.CartPage;
 import Pages.HomePage.HomePage;
 import Pages.HomePage.TopBars;
-import org.openqa.selenium.By;
+import Pages.Signup_LoginPage.SUpLogin;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import  utilities.TestBase;
-import static utilities.Locators.*;
-public class _16_TestCase extends TestBase {
+import utilities.TestBase;
 
-    @Test
-    public void placeOrderCheckout(){
-        HomePage homePage = new HomePage();
+public class _16_TestCase extends TestBase {
+    HomePage homePage;
+    CartPage cartPage;
+    SUpLogin sUpLogin;
+
+    @BeforeTest
+    public void initial() {
+        homePage = new HomePage();
+        cartPage = new CartPage();
+        sUpLogin = new SUpLogin();
         // 1. Launch browser
         // 2. Navigate to url 'http://automationexercise.com'
         homePage.navigateToSite();
         // 3. Verify that home page is visible successfully
         homePage.verifyMainPage();
-       //4. Click 'Signup / Login' button
+    }
+
+    @Test
+    public void signUp() {
+        //4. Click 'Signup / Login' button
         homePage.TopBarClicks(TopBars.SIGNUP_LOGIN);
-       //5. Fill all details in Signup and create account
-        newUserSignUp();
-       //6. Verify 'ACCOUNT CREATED!' and click 'Continue' button
-        verifyVisibility(lAccountCreatedText);
-click(By.linkText("Continue"));
-       //7. Verify ' Logged in as username' at top
-verifyVisibility(By.partialLinkText("Logged"));
-       //8. Add products to cartPage
-addProductsToCart(3);
-       //9. Click 'CartPage' button
-        click(lCart);
-       //10. Verify that cartPage page is displayed
-        verifyVisibility(lShoppingCartTxt);
+        //5. Fill all details in Signup and create account
+        sUpLogin.newUserSignUp();
+        //6. Verify 'ACCOUNT CREATED!' and click 'Continue' button
+        sUpLogin.verifyAccountCreated();
+        //7. Verify ' Logged in as username' at top
+        sUpLogin.verifyloggedAsUN();
+    }
+
+    @Test(dependsOnMethods = "signUp")
+    public void addProductAndOpenCartPage() {
+        //8. Add products to cartPage
+        homePage.addProductsToCart(3);
+        //9. Click 'CartPage' button
+        homePage.TopBarClicks(TopBars.CART);
+        //10. Verify that cartPage page is displayed
+        cartPage.verifyCartPage();
+    }
+
+    @Test(dependsOnMethods = "addProductAndOpenCartPage")
+    public void proceedToChckout() {
         //11. Click Proceed To Checkout
-        click(lProceedToChckout);
-       //12. Verify Address Details and Review Your Order
-        verifyAdressDetails();
-       //13. Enter description in comment text area and click 'Place Order'
-        sendKeys(lDescription, "bana tokanma");
-        click(lPlaceOrder);
-       //14. Enter payment details: Name on Card, Card Number, CVC, Expiration date
-        fillCreditCardCredentials();
-       //15. Click 'Pay and Confirm Order' button,
-       //16. Verify success message 'Your order has been placed successfully!'
-        driver.navigate().back();
-        verifyVisibility(By.cssSelector("div[id = 'success_message']"));
-        click(By.cssSelector("#submit"));
-        bekle(3);
-        click(By.xpath("//*[text()='Continue']"));
-       //17. Click 'Delete Account' button
-        click(By.xpath("//*[text()=' Delete Account']"));
-       //18. Verify 'ACCOUNT DELETED!' and click 'Continue' button
-        click(By.xpath("//*[text()='Account Deleted!']"));
+        cartPage.clickProceedToCheckOut();
+        //12. Verify Address Details and Review Your Order
+        sUpLogin.verifyAdressDetails();
+        //13. Enter description in comment text area and click 'Place Order'
+        cartPage.writeADescription("hela vela ");
+    }
+
+    @Test(dependsOnMethods = "proceedToChckout")
+    public void fillPaymentDetails() {
+        //14. Enter payment details: Name on Card, Card Number, CVC, Expiration date
+        cartPage.fillCreditCardCredentials();
+        //15. Click 'Pay and Confirm Order' button,
+        //16. Verify success message 'Your order has been placed successfully!'
+        cartPage.verifyYourOrderHasBeenPlaced();
+    }
+
+    @Test(dependsOnMethods = "fillPaymentDetails")
+    public void deleteAccount() {
+        //17. Click 'Delete Account' button
+        homePage.TopBarClicks(TopBars.DELETE_ACCOUNT);
+        //18. Verify 'ACCOUNT DELETED!' and click 'Continue' button
+        sUpLogin.verifyAccountDeleted();
     }
 
 }

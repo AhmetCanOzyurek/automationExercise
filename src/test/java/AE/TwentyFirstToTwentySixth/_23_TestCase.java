d64a49907b01b16d96e00d61ffff0cb176df39ef
AhmetCan <a.canozyurek@gmail.com>
2024-01-28 15:32:54 +0300

    Revisions for my automationExercise TestNG Project. part/27

--- src/test/java/AE/TwentyFirstToTwentySixth/_23_TestCase.java
+++ src/test/java/AE/TwentyFirstToTwentySixth/_23_TestCase.java
@@ -4,35 +4,49 @@ import Pages.HomePage.TopBars;
 import org.testng.annotations.Test;
 import utilities.TestBase;
 
-import static utilities.Locators.*;
-
 public class _23_TestCase extends TestBase {
     @Test
-    public void verifyAdressDetailsTest(){
+    public void openingSignUpPage() {
 //        4. Click 'Signup / Login' button
         TopBarClicks(TopBars.SIGNUP_LOGIN);
+    }
+    @Test(dependsOnMethods = "openingSignUpPage")
+    public void createNewUser() {
 //        5. Fill all details in Signup and create account
-        newUserSignUp();
+        sUpLogin.newUserSignUp();
 //        6. Verify 'ACCOUNT CREATED!' and click 'Continue' button
-verifyVisibility(lAccountCreatedText);
-//
-    click(lContinueAfterAccountCreated);
+        sUpLogin.verifyAccountCreated();
+    }
+    @Test(dependsOnMethods = "createNewUser")
+    public void verifyUsername(){
 //        7. Verify ' Logged in as username' at top
-verifyVisibility(lLoggedAsUsernameTxt);
+       sUpLogin.verifyloggedAsUN();
+    }
+    @Test (dependsOnMethods = "verifyUsername")
+    public void addProducts() {
 //        8. Add products to cart
-        addProductsToCart(3);
+        homePage.addProductsToCart(3);
 //        9. Click 'CartPage' button
-        click(lCart);
+        TopBarClicks(TopBars.CART);
+    }
+    @Test(dependsOnMethods = "addProducts")
+    public void verifyCartPage() {
 //        10. Verify that cart page is displayed
-        verifyVisibility(lShoppingCartTxt);
+        cartPage.verifyCartPage();
+    }
+    @Test(dependsOnMethods = "verifyCartPage")
+    public void verifyAddressDetails() {
 //        11. Click Proceed To Checkout
-        click(lProceedToChckout);
+        cartPage.clickProceedToCheckOut();
 //        12. Verify that the delivery address is same address filled at the time registration of account
-        verifyAdressDetails();
 //        13. Verify that the billing address is same address filled at the time registration of account
+        sUpLogin.verifyAdressDetails();
+    }
+    @Test(dependsOnMethods = "verifyAddressDetails")
+    public void deleteAccount(){
 //        14. Click 'Delete Account' button
-        click(lDeleteAccount);
+        TopBarClicks(TopBars.DELETE_ACCOUNT);
 //        15. Verify 'ACCOUNT DELETED!' and click 'Continue' button,
-        verifyVisibility(lAccountDeletedTxt);
+        sUpLogin.verifyAccountDeleted();
     }
 }
